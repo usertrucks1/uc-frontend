f1557f7d7d2da760d03bb1881574f2714660ace7
Alok Kumar Singh <usertrucks1@gmail.com>
2025-07-31 07:20:10 +0530

    header added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProvidersPage from './pages/ProviderPage';
 import SlotsPage from './pages/SlotsPage';
@@ -9,10 +9,20 @@ import BookingPage from './pages/BookingPage';
 const App = () => {
   return (
     <>
-      <header className="sticky top-0 z-50 bg-white shadow-md flex items-center p-4">
-        <img src="/logo.png" alt="App Icon" className="h-8 w-8 mr-2" />
-        <h1 className="text-lg font-semibold">Urban</h1>
+      <header className="sticky top-0 z-50 bg-white shadow-md flex items-center p-4 px-10">
+        <Link to="/" className="text-gray-700 hover:text-indigo-600">
+          <div className="flex items-center">
+            <img src="/logo.png" alt="App Icon" className="h-8 w-8 mr-2" />
+            <h1 className="text-lg font-semibold">Urban</h1>
+          </div>
+        </Link>
+
+        <nav className="ml-auto space-x-4">
+          <Link to="/" className="text-gray-700 hover:text-indigo-600">Home</Link>
+          <Link to="/bookings" className="text-gray-700 hover:text-indigo-600">Bookings</Link>
+        </nav>
       </header>
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/providers/:categoryId" element={<ProvidersPage />} />
